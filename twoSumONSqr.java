ef6375d5ac3488773831d98a615cc9a6b22668ee
Srihita Sai Ayinala <58342544+srihita30@users.noreply.github.com>
2022-05-24 13:35:01 +0530

    Given an array of int's called nums & an integer named target, we must return indices of the 2 numbers in nums array such that they sum to target.
    Check for edge cases: cannot use the same element twice.

---
+++ twoSumONSqr.java
@@ -0,0 +1,27 @@
+public class twoSumONSqr {
+
+    public int[] twoSum(int[] nums, int target) {
+
+        int[] targetArr = new int[2];
+
+        for (int i = 0; i < nums.length - 1; i++) {
+
+            // inner loop supports the edge case
+            // edge case: not accpeting the same index position twice
+
+            for(int j = i+1; j<nums.length; j++) {
+
+                if(nums[i] + nums[j] == target) {
+
+                    targetArr[0] = i;
+
+                    targetArr[1] = j;
+
+                }
+            }
+        }
+
+        return targetArr;
+
+    }
+}
